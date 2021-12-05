0cca9fb3af0c7682a5de008d99b48f9e8c9cca1b
Adarsh Kumar Gupta <59231700+adarshgupta400@users.noreply.github.com>
2021-12-05 23:30:23 +0530

    adding files of workspace

---
+++ script.js
@@ -0,0 +1,8 @@
+setInterval(function(){
+
+     var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+     if(skipButton != undefined && skipButton.length > 0){
+          console.log("Ad detected here");
+          skipButton[0].click();
+     }
+}, 3000)
